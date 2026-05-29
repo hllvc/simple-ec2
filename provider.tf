ab1c8f7c81d2280490ea06a6715b782694d49531
hllvc <github@hllvc.com>
2026-05-29 12:10:02 +0200

    Update lockfile.

--- provider.tf
+++ provider.tf
@@ -3,18 +3,18 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.14.0"
+      source = "hashicorp/aws"
+      # version = "~> 6.14.0"
     }
 
     random = {
-      source  = "hashicorp/random"
-      version = "~> 3.7.0"
+      source = "hashicorp/random"
+      # version = "~> 3.7.0"
     }
 
     tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.1.0"
+      source = "hashicorp/tls"
+      # version = "~> 4.1.0"
     }
   }
 }
